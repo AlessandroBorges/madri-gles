a4ae54bc21874b95bfd8a9dc8db0a1a84d066d95
JavaMonkey <AlessandroBorges@users.noreply.github.com>
2015-06-05 14:46:22 -0300

    Create BufferItem.java

---
+++ Canvas/src/android/graphics/BufferItem.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package android.graphics;
+
+/**
+ * @author Alessandro Borges
+ *
+ */
+public class BufferItem {
+
+    public int mBuf;
+    public Object mGraphicBuffer;
+    public Rect mCrop;
+    public int mTransform;
+    public int mScalingMode;
+    public long mTimestamp;
+    public long handle;
+
+}
