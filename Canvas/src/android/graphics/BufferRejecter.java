3af0ddaf9457fb771b6f40176fddd0d40e9dc87a
JavaMonkey <AlessandroBorges@users.noreply.github.com>
2015-06-05 14:47:58 -0300

    Create BufferRejecter.java

---
+++ Canvas/src/android/graphics/BufferRejecter.java
@@ -0,0 +1,14 @@
+package android.graphics;
+
+import java.util.Vector;
+
+import android.graphics.BufferQueue.BufferItem;
+
+public class BufferRejecter {
+
+    public boolean reject(Vector mGraphicBuffer, BufferItem item) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
