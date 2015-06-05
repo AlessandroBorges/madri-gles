be31a64f598da66505321df5cac08d769ea37a52
JavaMonkey <AlessandroBorges@users.noreply.github.com>
2015-06-05 14:43:25 -0300

    Update BufferQueue.java

--- Canvas/src/android/graphics/BufferQueue.java
+++ Canvas/src/android/graphics/BufferQueue.java
@@ -1,20 +1,11 @@
 package android.graphics;
 
-import android.graphics.BufferQueue.BufferItem;
+import android.graphics.BufferItem;
 import android.opengl.EGLDisplay;
 
-class BufferQueue {
-
-    public class BufferItem {
-
-        public int mBuf;
-        public Object mGraphicBuffer;
-        public Rect mCrop;
-        public int mTransform;
-        public int mScalingMode;
-        public long mTimestamp;
-
-    }
+@SuppressWarnings("serial")
+class BufferQueue extends java.util.Vector<BufferItem> {
+   
     public static final int STALE_BUFFER_SLOT = 0;
     public static final int INVALID_BUFFER_SLOT = 0;
     public int acquireBuffer(BufferItem item) {
